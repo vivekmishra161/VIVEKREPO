6146d254b6701e16a2a5d18eb0e3235da73f7192
vivekmishra161 <vivekmishra261105@gmail.com>
2026-02-01 17:05:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -643,14 +643,19 @@ app.post("/forgot-password", async (req, res) => {
       { resetToken: token, resetTokenExpiry: expiry },
       { where: { id: user.id } }
     );
+console.log("EMAIL USER:", process.env.EMAIL_USER);
+console.log("EMAIL PASS:", process.env.EMAIL_PASS);
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
 
     const resetLink = `https://akcautoparts.onrender.com/reset-password/${token}`;
 
